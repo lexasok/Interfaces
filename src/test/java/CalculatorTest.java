87e391df28b441ddab6faf5c20a568dc814682c3
lexa <sokolov@ozero.net>
2018-04-10 16:46:11 +0300

    Test written and failed

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
+    @Test
+    public void divisorSum() {
+        Calculator calculator = new Calculator();
+        assertEquals(12, calculator.divisorSum(6));
+    }
 }
