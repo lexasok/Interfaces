f70528d251f9a018e3e3ed28958d8f0d57bf51b5
lexa <sokolov@ozero.net>
2018-04-10 15:15:45 +0300

    Methods implemented

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -1,2 +1,11 @@
-public class Enemy {
+
+public class Enemy implements Character {
+
+    public void attack() {
+        System.out.println("Attack!");
+    }
+
+    public void heal() {
+        System.out.println("Heal!");
+    }
 }
