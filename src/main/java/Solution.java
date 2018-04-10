fe104a6871aee6ac82a2cdf8dcfb36bc40398faf
lexa <sokolov@ozero.net>
2018-04-10 16:34:43 +0300

    Program logic done

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 interface AdvencedArithmetic {
     int divisorSum(int n);
@@ -12,6 +13,13 @@ class Calculator implements AdvencedArithmetic {
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
 
+        AdvencedArithmetic myCalc = new Calculator();
+        int sum = myCalc.divisorSum(n);
+        System.out.println("I implemented: " + myCalc.getClass().getInterfaces()[0].getName());
+        System.out.println(sum);
     }
 }
