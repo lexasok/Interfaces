6e7a0232300b1c1b7f85d4551ab1ea0f53babc49
lexa <sokolov@ozero.net>
2018-04-10 15:25:23 +0300

    Methods implemented, constructor and field added

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,17 @@
-public class Hero {
+
+public class Hero implements Character {
+
+    String weapon = "Knife";
+
+    public Hero() {
+
+    }
+
+    public void attack() {
+        System.out.println("Attack!");
+    }
+
+    public void heal() {
+        System.out.println("Heal!");
+    }
 }
