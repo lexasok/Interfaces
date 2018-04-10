752fbe925fbd08fc1a1e762c579218fd31135bb9
lexa <sokolov@ozero.net>
2018-04-10 16:48:04 +0300

    Method implemented, tests passed

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -7,16 +7,15 @@ interface AdvencedArithmetic {
 class Calculator implements AdvencedArithmetic {
 
     public int divisorSum(int n) {
-//        int result = n;
-//        int i = n;
-//        while (i-- > 1) {
-//            if (n % i == 0) {
-//                result += i;
-//            }
-//        }
-//
-//        return result;
-        return 0;
+        int result = n;
+        int i = n;
+        while (i-- > 1) {
+            if (n % i == 0) {
+                result += i;
+            }
+        }
+
+        return result;
     }
 }
 
