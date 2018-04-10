6f3825161252ebef0a25986304883d34921c392b
lexa <sokolov@ozero.net>
2018-04-10 15:55:38 +0300

    Class created

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,7 +3,12 @@ interface AdvencedArithmetic {
     int divisorSum(int n);
 }
 
+class Calculator implements AdvencedArithmetic {
 
+    public int divisorSum(int n) {
+        return 0;
+    }
+}
 
 public class Solution {
     public static void main(String[] args) {
